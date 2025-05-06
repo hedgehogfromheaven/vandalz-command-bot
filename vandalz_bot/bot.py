f9e7ad4a7b6edfe3f91de4bfab5965aac03b4d5f
hedgehogfromheaven <113244009+hedgehogfromheaven@users.noreply.github.com>
2025-05-06 15:29:13 +0300

    c

--- vandalz_bot/bot.py
+++ vandalz_bot/bot.py
@@ -1,29 +1,14 @@
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram import Router
 import asyncio
-import logging
-import os
+from aiogram import Bot, Dispatcher
 
-from handlers import start, status, log_work
+# сюда вставляешь токен напрямую или из переменной окружения
+BOT_TOKEN = "8156758624:AAFcIcItUe7jK_XjLoFbtrvuHxjy7fWeTBU"
 
-# Настройка логгера
-logging.basicConfig(level=logging.INFO)
-
-# Конфигурация
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# Инициализация
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-# Роутеры
-dp.include_router(start.router)
-dp.include_router(status.router)
-dp.include_router(log_work.router)
-
-# Запуск бота
 async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    # тут твои хендлеры
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
